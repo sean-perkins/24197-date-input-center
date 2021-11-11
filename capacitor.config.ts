fbfa6aed8a4a3c0cdec1685d8e87ebb109fa8f58
Sean Perkins <sean@ionic.io>
2021-11-10 21:37:11 -0500

    chore(demo): replicate bug

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'date-input-cener',
+  appName: 'date-input-center',
   webDir: 'build',
   bundledWebRuntime: false
 };
--- src/components/ExploreContainer.tsx
+++ src/components/ExploreContainer.tsx
@@ -1,3 +1,4 @@
+import { IonInput, IonItem, IonLabel } from '@ionic/react';
 import './ExploreContainer.css';
 
 interface ContainerProps {
@@ -9,6 +10,10 @@ const ExploreContainer: React.FC<ContainerProps> = ({ name }) => {
     <div className="container">
       <strong>{name}</strong>
       <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+      <IonItem>
+        <IonLabel position='stacked'>Date</IonLabel>
+        <IonInput type='date' />
+      </IonItem>
     </div>
   );
 };
